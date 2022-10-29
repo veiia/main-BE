5badc95c9cf2b855c847a6395455a7483828ce40
syth0le <chdan565@gmail.com>
2022-10-29 14:17:36 +0300

    VEIIA-22: changes for flake8 and black

--- app/profile/routers/info.py
+++ app/profile/routers/info.py
@@ -1,29 +1,28 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
 from app.settings.tags import Tags
 
 router = APIRouter(
-    prefix=f'/{Tags.ME}',
+    prefix=f"/{Tags.ME}",
     tags=[Tags.PROFILE],
     # dependencies=[Depends(get_token_header)],
     responses={
-        404: {'description': 'Not found'},
-        403: {'description': 'permission denied'},
-    }
+        404: {"description": "Not found"},
+        403: {"description": "permission denied"},
+    },
 )
 
 
-@router.get('/')
+@router.get("/")
 async def get_profile_information():
-    return {'name': 'me get'}
+    return {"name": "me get"}
 
 
-@router.patch('/')
+@router.patch("/")
 async def edit_profile_information():
-    return {'name': 'me patch'}
+    return {"name": "me patch"}
 
 
-@router.delete('/')
+@router.delete("/")
 async def delete_profile():
-    return {'name': 'me delete'}
-
+    return {"name": "me delete"}
